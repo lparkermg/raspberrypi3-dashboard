d093b4d96fa28da210976be588262a2b5fe8bb79
Luke Parker <moongateuk@gmail.com>
2016-03-13 13:54:47 +0000

    Linked in the dashboard home view.

--- components/App/app.js
+++ components/App/app.js
@@ -1,5 +1,10 @@
 define(['libs/knockout'],
   function(ko){
+    ko.components.register('dashboard-home',{
+      viewModel: { require: 'components/Home/home'}
+      template: { require: 'text!components/Home/home.html'}
+    });
+    
     return function(){
       var self = this;
     };
