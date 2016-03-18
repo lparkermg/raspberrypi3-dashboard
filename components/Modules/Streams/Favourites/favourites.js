a0938a57c5d8b75a1e9050c2fee652de1ed80629
Luke Parker <moongateuk@gmail.com>
2016-03-18 15:55:27 +0000

    Modified streaming stuff.

--- components/Modules/Streams/Favourites/favourites.js
+++
@@ -1,9 +0,0 @@
-define(['libs/knockout'],
-  function StreamFavouritesViewModel(ko){
-    return function(){
-      var self = this;
-
-      //This will house the functions for the following.
-      //- Switching between favourite streams to play.
-    };
-  });
--- scripts/stats_gen.sh
+++ scripts/stats_gen.sh
@@ -6,7 +6,7 @@ upTimeDays=$(uptime | awk '{print $3}')
 upTimeHours=$(uptime | awk '{print $5}' | tr ',' '.')
 loadAverages=$(uptime | awk '{ print $10,$11,$12}')
 FILE="status.json"
-jsonStr="{\"devName\":\"${piHost}\",\"space\":\"${used} of ${size} (${usedPercent})\",\"hostUpTime\":\"${upTimeDays} Day(s) and ${upTimeHours}\",\"loadAverages\":\"${loadAverages}\",\"btDevStatus\":\"N/A\",\"connectedDevice\":\"N/A\",\"btConnTime\":\"N/A\"}"
+jsonStr="{\"devName\":\"${piHost}\",\"space\":\"${used} of ${size} (${usedPercent})\",\"hostUpTime\":\"${upTimeDays} Day(s) and ${upTimeHours}\",\"loadAverages\":\"${loadAverages}\"}"
 echo $jsonStr
 
 cd ../data/
